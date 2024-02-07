b59e3e2b0162a311c8a17937dc38b5cd70a2aa6a
tiagofmuniz <61099509+tiagofmuniz@users.noreply.github.com>
2024-02-07 20:35:51 -0300

    name correct

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {}
-=======
-const nextConfig = {
-    
-}
->>>>>>> 7632f75592102bb2e5fe16cd3353f15202bdc269
 
 module.exports = nextConfig
