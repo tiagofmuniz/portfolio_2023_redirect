27d7eac05c949421c3119f0170bcc852d8bd6472
tiagofmuniz <tiagofmuniz2023@gmail.com>
2024-02-08 12:00:25 -0300

    v2

--- src/components/page_containers/Skills/Skills.tsx
+++ src/components/page_containers/Skills/Skills.tsx
@@ -3,16 +3,9 @@ import styles from './Skills.module.scss';
 import WaterMark from 'components/ui/WaterMark/WaterMark';
 import Cube3d from './Cube3d/Cube3d';
 import 'react-tippy/dist/tippy.css';
-import { Tooltip } from 'react-tippy';
 import SectionObserver from 'contexts_and_providers/SectionObserver';
 import { skills } from 'app/api/(data)/skills';
 
-interface ISkillProps {
-  label: string;
-  level?: string;
-  tooltip?: string;
-  icon: React.ReactNode;
-}
 
 export default function Skills() {
 const SkillList = skills;
@@ -25,7 +18,7 @@ const SkillList = skills;
       <div className={styles.skillItem}>
         {SkillList?.map((item) => 
        (
-            <Tooltip key={item.label} title={item.tooltip} size='big' arrow={true} position='bottom' className={styles.customTooltip}>
+        
               <div className={styles.cubeContainer}>
                 <Cube3d
                   label={item.label}
@@ -35,7 +28,6 @@ const SkillList = skills;
                   skew={item.iconInternal.skew}
                 />
               </div>
-            </Tooltip>
           )
         )}
       </div>
