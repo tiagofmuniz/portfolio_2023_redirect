3e872bc663111e3477733051b7958fb10a9622c7
tiagofmuniz <tiagofmuniz2023@gmail.com>
2024-02-08 10:31:24 -0300

    v2

--- src/components/page_containers/Skills/Skills.tsx
+++ src/components/page_containers/Skills/Skills.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './Skills.module.scss';
 import WaterMark from 'components/ui/WaterMark/WaterMark';
 import Cube3d from './Cube3d/Cube3d';
-import { skills } from 'app/api/(data)/skills';
 import 'react-tippy/dist/tippy.css';
 import { Tooltip } from 'react-tippy';
 import SectionObserver from 'contexts_and_providers/SectionObserver';
+import { skills } from 'app/api/(data)/skills';
 
 interface ISkillProps {
   label: string;
@@ -15,7 +15,7 @@ interface ISkillProps {
 }
 
 export default function Skills() {
-
+const SkillList = skills;
   return (
     <div className={styles.skills_container}>
       <SectionObserver sectionId='skills'>
@@ -23,7 +23,7 @@ export default function Skills() {
       </SectionObserver>
 
       <div className={styles.skillItem}>
-        {skills?.map((item) => 
+        {SkillList?.map((item) => 
        (
             <Tooltip key={item.label} title={item.tooltip} size='big' arrow={true} position='bottom' className={styles.customTooltip}>
               <div className={styles.cubeContainer}>
